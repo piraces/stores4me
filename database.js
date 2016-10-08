367e8786188aac59b2f6e68fccd372b027dfb6db
Raul Piraces <raul.piraces@gmail.com>
2016-10-09 01:46:59 +0200

    Better database management.

--- database.js
+++ database.js
@@ -7,37 +7,45 @@ var mongoose = require('mongoose'),
     Transactions = require("./models/result.js").Transactions;
 
 // URL de conexión
-function readFromDatabaseByAge(age, callback) {
+function readFromDatabaseAge(age, callback) {
     Customers.find({'customer.age': age}).exec(function (err, cust) {
-        if (err){
+        if (err) {
             callback({'error': 500, 'errorDetail': 'Error en la recuperación de resultados.'});
         } else {
             var validTransactions = {};
             var numberOfIterations = 0;
             var currentArray = [];
-            cust.forEach(function(customer){
-                if(customer.contracts.credit_cards) {
-                    var current = customer.contracts.credit_cards.toObject();
-                    for(var i=0; i<current.length; i++) {
-                        currentArray.push(current[i].id);
-                    }
-                    current = customer.contracts.debit_cards.toObject();
-                    for(var i=0; i<current.length; i++) {
-                        currentArray.push(current[i].id);
-                    }
-                    current = customer.contracts.accounts.toObject();
-                    for(var i=0; i<current.length; i++) {
+            cust.forEach(function (customer) {
+                var current = [];
+                if (customer.contracts.credit_cards) {
+                    current = customer.contracts.credit_cards.toObject();
+                    for (var i = 0; i < current.length; i++) {
                         currentArray.push(current[i].id);
                     }
                 }
-                if(numberOfIterations == cust.length-1){
-                    Transactions.find({'accountId': {$in: currentArray}, peerLocation:{$exists:true}}).exec(function (err, documents) {
+                if (customer.contracts.debit_cards) {
+                    current = customer.contracts.debit_cards.toObject();
+                    for (var i = 0; i < current.length; i++) {
+                        currentArray.push(current[i].id);
+                    }
+                }
+                if (customer.contracts.accounts) {
+                    current = customer.contracts.accounts.toObject();
+                    for (var i = 0; i < current.length; i++) {
+                        currentArray.push(current[i].id);
+                    }
+                }
+                if (numberOfIterations == cust.length - 1) {
+                    Transactions.find({
+                        'accountId': {$in: currentArray},
+                        peerLocation: {$exists: true}
+                    }).exec(function (err, documents) {
                         if (err) {
                             callback({'error': 500, 'errorDetail': 'Error en la recuperación de resultados.'});
                         } else {
                             var iterations = 0;
                             documents.forEach(function (document) {
-                                if (validTransactions[document.peerLocation.lat.toFixed(2) + "," + document.peerLocation.lon.toFixed(2)]){
+                                if (validTransactions[document.peerLocation.lat.toFixed(2) + "," + document.peerLocation.lon.toFixed(2)]) {
                                     validTransactions[document.peerLocation.lat.toFixed(2) + "," + document.peerLocation.lon.toFixed(2)].intensity =
                                         validTransactions[document.peerLocation.lat.toFixed(2) + "," + document.peerLocation.lon.toFixed(2)].intensity + 1;
                                 } else {
@@ -47,8 +55,73 @@ function readFromDatabaseByAge(age, callback) {
                                         intensity: 0
                                     };
                                 }
-                                if(iterations == documents.length-1){
-                                    callback(validTransactions);
+                                if (iterations == documents.length - 1) {
+                                    var finalArray = [];
+                                    for(var i in validTransactions){
+                                        finalArray.push(validTransactions[i]);
+                                    }
+                                    callback(finalArray);
+                                }
+                                iterations++;
+                            });
+                        }
+                    });
+                }
+                numberOfIterations++;
+            });
+        }
+    });
+}
+
+function readFromDatabaseAgeAmount(age, amount, callback) {
+    Customers.find({'customer.age': age}).exec(function (err, cust) {
+        if (err) {
+            callback({'error': 500, 'errorDetail': 'Error en la recuperación de resultados.'});
+        } else {
+            var validTransactions = {};
+            var numberOfIterations = 0;
+            var currentArray = [];
+            cust.forEach(function (customer) {
+                if (customer.contracts.credit_cards) {
+                    var current = customer.contracts.credit_cards.toObject();
+                    for (var i = 0; i < current.length; i++) {
+                        currentArray.push(current[i].id);
+                    }
+                    current = customer.contracts.debit_cards.toObject();
+                    for (var i = 0; i < current.length; i++) {
+                        currentArray.push(current[i].id);
+                    }
+                    current = customer.contracts.accounts.toObject();
+                    for (var i = 0; i < current.length; i++) {
+                        currentArray.push(current[i].id);
+                    }
+                }
+                if (numberOfIterations == cust.length - 1) {
+                    Transactions.find({
+                        'accountId': {$in: currentArray},
+                        peerLocation: {$exists: true}
+                    }).exec(function (err, documents) {
+                        if (err) {
+                            callback({'error': 500, 'errorDetail': 'Error en la recuperación de resultados.'});
+                        } else {
+                            var iterations = 0;
+                            documents.forEach(function (document) {
+                                if (validTransactions[document.peerLocation.lat.toFixed(2) + "," + document.peerLocation.lon.toFixed(2)]) {
+                                    validTransactions[document.peerLocation.lat.toFixed(2) + "," + document.peerLocation.lon.toFixed(2)].intensity =
+                                        validTransactions[document.peerLocation.lat.toFixed(2) + "," + document.peerLocation.lon.toFixed(2)].intensity + 1;
+                                } else {
+                                    validTransactions[document.peerLocation.lat.toFixed(2) + "," + document.peerLocation.lon.toFixed(2)] = {
+                                        lat: document.peerLocation.lat.toFixed(2),
+                                        lon: document.peerLocation.lon.toFixed(2),
+                                        intensity: 0
+                                    };
+                                }
+                                if (iterations == documents.length - 1) {
+                                    var finalArray = [];
+                                    for(var i in validTransactions){
+                                        finalArray.push(validTransactions[i]);
+                                    }
+                                    callback(finalArray);
                                 }
                                 iterations++;
                             });
@@ -62,4 +135,5 @@ function readFromDatabaseByAge(age, callback) {
 }
 
 // Funciones exportadas para ser usadas externamente
-exports.readFromDatabaseByAge = readFromDatabaseByAge;
+exports.readFromDatabaseAge = readFromDatabaseAge;
+exports.readFromDatabaseAgeAmount = readFromDatabaseAgeAmount;
--- routes/map.js
+++ routes/map.js
@@ -3,7 +3,7 @@ module.exports = function(app) {
     app.get("/heat/:id", function (req, response) {
         var db = require('../database');
         // Lectura de todas las tareas de la BD
-        db.readFromDatabaseByAge(req.params.id, function (rows) {
+        db.readFromDatabaseAge(req.params.id, function (rows) {
             if (typeof(rows) == "object" && rows != null && !rows.hasOwnProperty('error')) {
                 response.writeHead(200, {"Content-Type": "application/json"});
                 response.write(JSON.stringify(rows));
@@ -22,5 +22,26 @@ module.exports = function(app) {
         });
     });
 
+    app.get("/heat/:id/:amount", function (req, response) {
+        var db = require('../database');
+        // Lectura de todas las tareas de la BD
+        db.readFromDatabaseAgeAmount(req.params.id, req.params.amount, function (rows) {
+            if (typeof(rows) == "object" && rows != null && !rows.hasOwnProperty('error')) {
+                response.writeHead(200, {"Content-Type": "application/json"});
+                response.write(JSON.stringify(rows));
+                response.end();
+            } else if (rows.hasOwnProperty('error')) {
+                // error case
+                response.writeHead(500, {"Content-Type": "text/html"});
+                response.write("Fatal error");
+                response.end();
+            } else {
+                // error case
+                response.writeHead(500, {"Content-Type": "text/html"});
+                response.write("Fatal error");
+                response.end();
+            }
+        });
+    });
 
 };
