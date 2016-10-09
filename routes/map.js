c70d78fcdf9adaf4690042d1411105728467d871
Raul Piraces <raul.piraces@gmail.com>
2016-10-09 02:17:35 +0200

    Implemented more features.

--- routes/map.js
+++ routes/map.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
         var db = require('../database');
         // Lectura de todas las tareas de la BD
         if(req.params.id.includes("-")){
-            db.readFromDatabaseAgeInterval(req.params.id.split("-")[0], req.params.id.split("-")[0], function (rows) {
+            db.readFromDatabaseAgeInterval(req.params.id.split("-")[0], req.params.id.split("-")[1], function (rows) {
                 if (typeof(rows) == "object" && rows != null && !rows.hasOwnProperty('error')) {
                     response.writeHead(200, {"Content-Type": "application/json"});
                     response.write(JSON.stringify(rows));
@@ -44,24 +44,43 @@ module.exports = function(app) {
 
     app.get("/heat/:id/:amount", function (req, response) {
         var db = require('../database');
-        // Lectura de todas las tareas de la BD
-        db.readFromDatabaseAgeAmount(req.params.id, req.params.amount, function (rows) {
-            if (typeof(rows) == "object" && rows != null && !rows.hasOwnProperty('error')) {
-                response.writeHead(200, {"Content-Type": "application/json"});
-                response.write(JSON.stringify(rows));
-                response.end();
-            } else if (rows.hasOwnProperty('error')) {
-                // error case
-                response.writeHead(500, {"Content-Type": "text/html"});
-                response.write("Fatal error");
-                response.end();
-            } else {
-                // error case
-                response.writeHead(500, {"Content-Type": "text/html"});
-                response.write("Fatal error");
-                response.end();
-            }
-        });
+        if(req.params.id.includes("-")){
+            db.readFromDatabaseAgeIntervalAmount(req.params.id.split("-")[0], req.params.id.split("-")[1], function (rows) {
+                if (typeof(rows) == "object" && rows != null && !rows.hasOwnProperty('error')) {
+                    response.writeHead(200, {"Content-Type": "application/json"});
+                    response.write(JSON.stringify(rows));
+                    response.end();
+                } else if (rows.hasOwnProperty('error')) {
+                    // error case
+                    response.writeHead(500, {"Content-Type": "text/html"});
+                    response.write("Fatal error");
+                    response.end();
+                } else {
+                    // error case
+                    response.writeHead(500, {"Content-Type": "text/html"});
+                    response.write("Fatal error");
+                    response.end();
+                }
+            });
+        } else {
+            // Lectura de todas las tareas de la BD
+            db.readFromDatabaseAgeAmount(req.params.id, req.params.amount, function (rows) {
+                if (typeof(rows) == "object" && rows != null && !rows.hasOwnProperty('error')) {
+                    response.writeHead(200, {"Content-Type": "application/json"});
+                    response.write(JSON.stringify(rows));
+                    response.end();
+                } else if (rows.hasOwnProperty('error')) {
+                    // error case
+                    response.writeHead(500, {"Content-Type": "text/html"});
+                    response.write("Fatal error");
+                    response.end();
+                } else {
+                    // error case
+                    response.writeHead(500, {"Content-Type": "text/html"});
+                    response.write("Fatal error");
+                    response.end();
+                }
+            });
+        }
     });
-
 };
